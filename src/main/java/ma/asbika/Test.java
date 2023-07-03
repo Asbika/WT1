67a815dce493b7db11785e9e952784819dc4a780
unknown <hicham.asbika.e@gmail.com>
2023-07-03 18:00:58 +0100

    comments

--- src/main/java/ma/asbika/Test.java
+++ src/main/java/ma/asbika/Test.java
@@ -1,7 +1,10 @@
 package ma.asbika;
-
+/**
+ * Comment
+ */
 public class Test{
     public static void main(String[] args){
+        //--------------------------------------
         System.out.println("Hello!");
     }
 }
